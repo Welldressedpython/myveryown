0745816ae4867b6f05c7b6343a576f8c465b776e
Welldressedpython <137125172+Welldressedpython@users.noreply.github.com>
2023-06-19 19:28:45 -0700

    ex1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#("Hello World!")
+print("Hello Again")
+("I like typing this.")
+("This is fun.")
+('Yay! Printing.')
+("I'd much rather you 'not'.")
+('I "said" do not touch this.')
+("this is my 'new' line.")
